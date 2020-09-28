1828da839854fa9a2662dfb8ad2d7cbbb7d7fc25
bowei <bowei.han@cienet.com.cn>
2020-09-28 17:12:52 +0800

    add 117

---
+++ src/main/java/algorithms/medium/PopulatingNextRightPointersInEachNodeII117.java
@@ -0,0 +1,73 @@
+package algorithms.medium;
+
+import Bean.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/
+ */
+public class PopulatingNextRightPointersInEachNodeII117 {
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Node last = null;
+            for (int i = 1; i <= n; ++i) {
+                Node f = queue.poll();
+                if (f.left != null) {
+                    queue.offer(f.left);
+                }
+                if (f.right != null) {
+                    queue.offer(f.right);
+                }
+                if (i != 1) {
+                    last.next = f;
+                }
+                last = f;
+            }
+        }
+        return root;
+    }
+
+    Node last = null, nextStart = null;
+
+    /**
+     * use the pre-setup pointers
+     * @param root
+     * @return
+     */
+    public Node connectII(Node root) {
+        Node start = root;
+        while (start != null) {
+            last = null;
+            nextStart = null;
+            for (Node p = start; p != null; p = p.next) {
+                if (p.left != null) {
+                    handle(p.left);
+                }
+                if (p.right != null) {
+                    handle(p.right);
+                }
+            }
+            start = nextStart;
+        }
+        return root;
+    }
+
+    private void handle(Node p) {
+        if (last != null) {
+            last.next = p;
+        }
+        if (nextStart == null) {
+            nextStart = p;
+        }
+        last = p;
+    }
+}
