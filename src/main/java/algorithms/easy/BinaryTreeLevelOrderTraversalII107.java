a9f795fd2eb9a9054cccdf2859da5098a15f2c2c
unknown <remnantwill@163.com>
2020-09-29 16:29:00 +0800

    add 107

---
+++ src/main/java/algorithms/easy/BinaryTreeLevelOrderTraversalII107.java
@@ -0,0 +1,41 @@
+package algorithms.easy;
+
+import Bean.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/
+ */
+public class BinaryTreeLevelOrderTraversalII107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levelOrder = new LinkedList<>();
+        if (root == null) {
+            return levelOrder;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            levelOrder.add(0, level);
+        }
+
+        return levelOrder;
+    }
+
+}
