f2b326bae517f342ba38d412a9c93f2cf122dbe4
remnantwill <remnantwill@163.com>
2020-10-07 23:50:31 +0800

    add several

---
+++ src/main/java/algorithms/medium/SortColors75.java
@@ -0,0 +1,88 @@
+package algorithms.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/sort-colors/
+ */
+public class SortColors75 {
+
+    /**
+     * single pointer solution
+     *
+     * @param nums
+     */
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int ptr = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ptr++;
+            }
+        }
+        for (int i = ptr; i < n; i++) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = temp;
+                ptr++;
+            }
+        }
+    }
+
+    /**
+     * double pointer solution
+     *
+     * @param nums
+     */
+    public void sortColorsII(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                int temp = nums[i];
+                nums[i] = nums[p1];
+                nums[p1] = temp;
+                ++p1;
+            } else if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                if (p0 < p1) {
+                    temp = nums[i];
+                    nums[i] = nums[p1];
+                    nums[p1] = temp;
+                }
+                ++p0;
+                ++p1;
+            }
+        }
+    }
+
+    /**
+     * double pointer solution II
+     * @param nums
+     */
+    public void sortColorsIII(int[] nums) {
+        int n = nums.length;
+        int p0 = 0, p2 = n - 1;
+        for (int i = 0; i <= p2; i++) {
+            while (i <= p2 && nums[i] == 2) {
+                int temp = nums[i];
+                nums[i] = nums[p2];
+                nums[p2] = temp;
+                --p2;
+            }
+            if (nums[i] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = temp;
+                ++p0;
+            }
+        }
+    }
+}
