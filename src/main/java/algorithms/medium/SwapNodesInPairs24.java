dd15590c992516c6aefee0244553ad7f256a3597
remnantwill <remnantwill@163.com>
2020-10-13 22:09:42 +0800

    add 24

---
+++ src/main/java/algorithms/medium/SwapNodesInPairs24.java
@@ -0,0 +1,45 @@
+package algorithms.medium;
+
+import Bean.ListNode;
+
+/**
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ */
+public class SwapNodesInPairs24 {
+
+    /**
+     * recursion solution
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    /**
+     * iteration solution
+     * @param head
+     * @return
+     */
+    public ListNode swapPairsII(ListNode head) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+
+}
