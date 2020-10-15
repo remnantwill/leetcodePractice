7e6a404241d191135739965ee7bb8da343b3b486
bowei <bowei.han@cienet.com.cn>
2020-10-15 18:02:04 +0800

    add 116

---
+++ src/main/java/algorithms/medium/PopulatingNextRightPointersInEachNode116.java
@@ -0,0 +1,42 @@
+package algorithms.medium;
+
+import Bean.Node;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+ */
+public class PopulatingNextRightPointersInEachNode116 {
+
+    /**
+     * Level traversal solution
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
+
+}
