0517c665c435e95114f6621d1c6d3772a7327f00
bowei <bowei.han@cienet.com.cn>
2020-10-20 18:00:48 +0800

    add 844

---
+++ src/main/java/algorithms/easy/BackspaceStringCompare844.java
@@ -0,0 +1,79 @@
+package algorithms.easy;
+
+/**
+ * https://leetcode-cn.com/problems/backspace-string-compare/
+ */
+public class BackspaceStringCompare844 {
+
+    /**
+     * refactor string solution
+     * @param S
+     * @param T
+     * @return
+     */
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String str) {
+        StringBuffer ret = new StringBuffer();
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            char c = str.charAt(i);
+            if (c != '#') {
+                ret.append(c);
+            } else {
+                if (ret.length() > 0) {
+                    ret.deleteCharAt(ret.length() - 1);
+                }
+            }
+        }
+        return ret.toString();
+    }
+
+    /**
+     * double pointer solution
+     * @param S
+     * @param T
+     * @return
+     */
+    public boolean backspaceCompareII(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            if (i >=0 && j >= 0) {
+                if (S.charAt(i) != T.charAt(j)) {
+                    return false;
+                }
+            } else if (i >= 0 || j >= 0) {
+                return false;
+            }
+            i--;
+            j--;
+        }
+        return true;
+    }
+
+}
