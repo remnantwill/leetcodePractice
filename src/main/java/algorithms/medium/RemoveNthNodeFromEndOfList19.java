b5ec9bb54bd1ad3182214f91191f99a53b2e954b
bowei <bowei.han@cienet.com.cn>
2020-10-21 10:45:51 +0800

    update 19

--- src/main/java/algorithms/medium/RemoveNthNodeFromEndOfList19.java
+++ src/main/java/algorithms/medium/RemoveNthNodeFromEndOfList19.java
@@ -2,11 +2,20 @@ package algorithms.medium;
 
 import Bean.ListNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/
  */
 public class RemoveNthNodeFromEndOfList19 {
 
+    /**
+     * calculate list length solution
+     * @param head
+     * @param n
+     * @return
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummyNode = new ListNode(0, head);
         int length = nodeLength(head);
@@ -29,4 +38,48 @@ public class RemoveNthNodeFromEndOfList19 {
         return length;
     }
 
+    /**
+     * stack solution
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEndII(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        Deque<ListNode> stack = new LinkedList<>();
+        ListNode cur = dummy;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        for (int i = 0; i < n; i++) {
+            stack.pop();
+        }
+        ListNode prev = stack.peek();
+        prev.next = prev.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+    /**
+     * one time traversal solution
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEndIII(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
 }
