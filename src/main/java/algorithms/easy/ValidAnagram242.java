6f4f54e4f00c88f2b9c6f19142fd88e856ac4112
bowei <bowei.han@cienet.com.cn>
2020-11-24 16:26:56 +0800

    add 242

---
+++ src/main/java/algorithms/easy/ValidAnagram242.java
@@ -0,0 +1,56 @@
+package algorithms.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/valid-anagram/
+ */
+public class ValidAnagram242 {
+
+    /**
+     * solution I: sorting
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        char[] chars1 = t.toCharArray();
+        Arrays.sort(chars);
+        Arrays.sort(chars1);
+        return Arrays.equals(chars, chars1);
+    }
+
+    /**
+     * solution II: hash
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagramII(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) - 1);
+            if (map.get(ch) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
