9c939cabd7ecfd09c538a00d9c65757e2236e6f6
bowei <bowei.han@cienet.com.cn>
2020-12-08 16:24:25 +0800

    update 767

--- src/main/java/algorithms/medium/ReorganizeString767.java
+++ src/main/java/algorithms/medium/ReorganizeString767.java
@@ -1,14 +1,55 @@
 package algorithms.medium;
 
+import java.util.PriorityQueue;
+
 /**
  * https://leetcode-cn.com/problems/reorganize-string/
  */
 public class ReorganizeString767 {
 
     public String reorganizeString(String S) {
-
-
-        return null;
+        if (S.length() < 2) {
+            return S;
+        }
+        int[] counts = new int[26];
+        int maxCount = 0;
+        int length = S.length();
+        for (int i = 0; i < length; i++) {
+            char c = S.charAt(i);
+            counts[c - 'a']++;
+            maxCount = Math.max(maxCount, counts[c - 'a']);
+        }
+        if (maxCount > (1 + length) / 2) {
+            return "";
+        }
+        PriorityQueue<Character> queue = new PriorityQueue<>(
+                (letter1, letter2) -> counts[letter2 - 'a'] - counts[letter1 - 'a']);
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (counts[c - 'a'] > 0) {
+                queue.offer(c);
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        while (queue.size() > 1) {
+            char letter1 = queue.poll();
+            char letter2 = queue.poll();
+            sb.append(letter1);
+            sb.append(letter2);
+            int index1 = letter1 - 'a';
+            int index2 = letter2 - 'a';
+            counts[index1]--;
+            counts[index2]--;
+            if (counts[index1] > 0) {
+                queue.offer(letter1);
+            }
+            if (counts[index2] > 0) {
+                queue.offer(letter2);
+            }
+        }
+        if (queue.size() > 0) {
+            sb.append(queue.poll());
+        }
+        return sb.toString();
     }
 
 }
---
+++ src/test/java/algorithms/medium/ReorganizeString767Test.java
@@ -0,0 +1,22 @@
+package algorithms.medium;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = ReorganizeString767.class)
+@RunWith(SpringRunner.class)
+public class ReorganizeString767Test {
+
+    @Autowired
+    private ReorganizeString767 instance;
+
+    @Test
+    public void reorganizeStringTest() {
+        String S = "aaababaacbb";
+        String ans = instance.reorganizeString(S);
+        System.out.println(ans);
+    }
+}
