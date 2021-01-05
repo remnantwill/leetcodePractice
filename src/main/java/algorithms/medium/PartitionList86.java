602c5ed4b51509c1815e4062cfe057c43a28eb1c
bowei <bowei.han@cienet.com.cn>
2021-01-05 15:45:08 +0800

    add 86

---
+++ src/main/java/algorithms/medium/PartitionList86.java
@@ -0,0 +1,30 @@
+package algorithms.medium;
+
+import Bean.ListNode;
+
+/**
+ * https://leetcode-cn.com/problems/partition-list/
+ */
+public class PartitionList86 {
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
+    }
+
+}
