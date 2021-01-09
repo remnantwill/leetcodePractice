c31eb7c0d0993e65b4e008a3011adf5523eed13e
remnantwill <remnantwill@163.com>
2021-01-09 17:06:19 +0800

    update 239

--- src/main/java/algorithms/hard/SlidingWindowMaximum239.java
+++ src/main/java/algorithms/hard/SlidingWindowMaximum239.java
@@ -65,4 +65,35 @@ public class SlidingWindowMaximum239 {
         return ans;
     }
 
+    /**
+     * solution III: Blocking + preprocessing
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindowIII(int[] nums, int k) {
+        int n = nums.length;
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i % k == 0) {
+                prefixMax[i] = nums[i];
+            } else {
+                prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1 || (i + 1) % k == 0) {
+                suffixMax[i] = nums[i];
+            } else {
+                suffixMax[i] = Math.max(nums[i], suffixMax[i + 1]);
+            }
+        }
+        int[] ans = new int[n - k + 1];
+        for (int i = 0; i <= (n - k); i++) {
+            ans[i] = Math.max(suffixMax[i], prefixMax[i + k - 1]);
+        }
+        return ans;
+    }
+
 }
