916a8c6b9802ac3a513b10e0d9e0cd7393109d38
bowei <bowei.han@cienet.com.cn>
2021-01-20 14:05:18 +0800

    add 721

---
+++ src/main/java/algorithms/medium/AccountsMerge721.java
@@ -0,0 +1,77 @@
+package algorithms.medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/accounts-merge/
+ */
+public class AccountsMerge721 {
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, Integer> emailsToIndex = new HashMap<>();
+        Map<String, String> emailsToName = new HashMap<>();
+        int emailCount = 0;
+        for (List<String> account : accounts) {
+            String name = account.get(0);
+            int size = account.size();
+            for (int i = 1; i < size; i++) {
+                String email = account.get(i);
+                if (!emailsToIndex.containsKey(email)) {
+                    emailsToIndex.put(email, emailCount++);
+                    emailsToName.put(email, name);
+                }
+            }
+        }
+        UnionFind uf = new UnionFind(emailCount);
+        for (List<String> account : accounts) {
+            String email = account.get(1);
+            Integer index = emailsToIndex.get(email);
+            int size = account.size();
+            for (int i = 2; i < size; i++) {
+                String nextEmail = account.get(i);
+                Integer nextIndex = emailsToIndex.get(nextEmail);
+                uf.union(index, nextIndex);
+            }
+        }
+        Map<Integer, List<String>> indexToEmails = new HashMap<>();
+        for (String email : emailsToIndex.keySet()) {
+            int index = uf.find(emailsToIndex.get(email));
+            List<String> account = indexToEmails.getOrDefault(index, new ArrayList<>());
+            account.add(email);
+            indexToEmails.put(index, account);
+        }
+        List<List<String>> merged = new ArrayList<>();
+        for (List<String> emails : indexToEmails.values()) {
+            List<String> account = new ArrayList<>();
+            Collections.sort(emails);
+            String name = emailsToName.get(emails.get(0));
+            account.add(name);
+            account.addAll(emails);
+            merged.add(account);
+        }
+        return merged;
+    }
+
+}
+
+class UnionFind {
+    int[] parent;
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public void union(int index1, int index2) {
+        parent[find(index2)] = find(index1);
+    }
+
+    public int find(int x) {
+        return parent[x] == x ? parent[x] : (parent[x] = find(parent[x]));
+    }
+}
