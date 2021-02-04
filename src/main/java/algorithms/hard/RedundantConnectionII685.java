6405001cabe9a5bbced2ebf2df75ddf94321a3bf
bowei <bowei.han@cienet.com.cn>
2021-02-04 10:59:55 +0800

    update 685

--- src/main/java/algorithms/hard/RedundantConnectionII685.java
+++ src/main/java/algorithms/hard/RedundantConnectionII685.java
@@ -42,26 +42,28 @@ public class RedundantConnectionII685 {
             }
         }
     }
-}
 
-class UnionFind {
-    int[] ancestor;
+    private class UnionFind {
+        int[] ancestor;
 
-    public UnionFind(int n) {
-        ancestor = new int[n];
-        for (int i = 0; i < n; ++i) {
-            ancestor[i] = i;
+        public UnionFind(int n) {
+            ancestor = new int[n];
+            for (int i = 0; i < n; ++i) {
+                ancestor[i] = i;
+            }
+        }
+
+        public void union(int index1, int index2) {
+            ancestor[find(index1)] = find(index2);
+        }
+
+        public int find(int index) {
+            if (ancestor[index] != index) {
+                ancestor[index] = find(ancestor[index]);
+            }
+            return ancestor[index];
         }
     }
-
-    public void union(int index1, int index2) {
-        ancestor[find(index1)] = find(index2);
-    }
-
-    public int find(int index) {
-        if (ancestor[index] != index) {
-            ancestor[index] = find(ancestor[index]);
-        }
-        return ancestor[index];
-    }
 }
+
+
