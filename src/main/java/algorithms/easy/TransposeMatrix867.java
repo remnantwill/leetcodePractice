5c3ece021d1f4c0a8c07476b899d5f990c710ff0
bowei <bowei.han@cienet.com.cn>
2021-02-25 17:23:23 +0800

    add 867

---
+++ src/main/java/algorithms/easy/TransposeMatrix867.java
@@ -0,0 +1,20 @@
+package algorithms.easy;
+
+/**
+ * https://leetcode-cn.com/problems/transpose-matrix/
+ */
+public class TransposeMatrix867 {
+
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] transposed = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+
+}
