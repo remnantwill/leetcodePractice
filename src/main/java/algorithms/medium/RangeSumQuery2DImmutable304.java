c33930ef1c577956a577b375a13e34ee3828bec5
bowei <bowei.han@cienet.com.cn>
2021-03-02 11:44:31 +0800

    add 304

---
+++ src/main/java/algorithms/medium/RangeSumQuery2DImmutable304.java
@@ -0,0 +1,62 @@
+package algorithms.medium;
+
+/**
+ * https://leetcode-cn.com/problems/range-sum-query-2d-immutable/
+ */
+public class RangeSumQuery2DImmutable304 {
+
+}
+
+/**
+ * solution I: one-dimensional prefix sum
+ */
+class NumMatrix {
+
+    int[][] sums;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            sums = new int[m][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i][j + 1] = sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            sum += sums[i][col2 + 1] - sums[i][col1];
+        }
+        return sum;
+    }
+}
+
+/**
+ * solution II: two-dimensional prefix sum
+ */
+class NumMatrixII {
+
+    int[][] sums;
+
+    public NumMatrixII(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            sums = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
+    }
+}
