d89ac9391900686e3b7a3d700fe676407cfbf2eb
bowei <bowei.han@cienet.com.cn>
2021-03-12 20:23:57 +0800

    add 331

---
+++ src/main/java/algorithms/medium/VerifyPreorderSerializationOfABinaryTree331.java
@@ -0,0 +1,75 @@
+package algorithms.medium;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * https://leetcode-cn.com/problems/verify-preorder-serialization-of-a-binary-tree/
+ */
+public class VerifyPreorderSerializationOfABinaryTree331 {
+
+    /**
+     * solution I: stack
+     * @param preorder
+     * @return
+     */
+    public boolean isValidSerialization(String preorder) {
+        int n = preorder.length();
+        int i = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(1);
+        while (i < n) {
+            if (stack.isEmpty()) {
+                return false;
+            }
+            if (preorder.charAt(i) == ',') {
+                i++;
+            } else if (preorder.charAt(i) == '#') {
+                int top = stack.pop() - 1;
+                if (top > 0) {
+                    stack.push(top);
+                }
+                i++;
+            } else {
+                while (i < n && preorder.charAt(i) != ',') {
+                    i++;
+                }
+                int top = stack.pop() - 1;
+                if (top > 0) {
+                    stack.push(top);
+                }
+                stack.push(2);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    /**
+     * solution II: counter
+     * @param preorder
+     * @return
+     */
+    public boolean isValidSerializationII(String preorder) {
+        int n = preorder.length();
+        int i = 0;
+        int slots = 1;
+        while (i < n) {
+            if (slots == 0) {
+                return false;
+            }
+            if (preorder.charAt(i) == ',') {
+                i++;
+            } else if (preorder.charAt(i) == '#') {
+                slots--;
+                i++;
+            } else {
+                while (i < n && preorder.charAt(i) != ',') {
+                    i++;
+                }
+                slots++;
+            }
+        }
+        return slots == 0;
+    }
+
+}
