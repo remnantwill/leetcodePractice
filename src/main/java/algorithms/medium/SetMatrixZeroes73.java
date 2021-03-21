0cd2247cb090d8effb722513142550e54c27dedc
remnantwill <remnantwill@163.com>
2021-03-21 17:28:52 +0800

    add 73

---
+++ src/main/java/algorithms/medium/SetMatrixZeroes73.java
@@ -0,0 +1,108 @@
+package algorithms.medium;
+
+/**
+ * https://leetcode-cn.com/problems/set-matrix-zeroes/
+ */
+public class SetMatrixZeroes73 {
+
+    /**
+     * solution I: using marking array
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    /**
+     * solution II: using two marking variables
+     * @param matrix
+     */
+    public void setZeroesII(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean flagCol0 = false;
+        boolean flagRow0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                flagCol0 = true;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                flagRow0 = true;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (flagCol0) {
+                matrix[i][0] = 0;
+            }
+        }
+        for (int j = 0; j < n; j++) {
+            if (flagRow0) {
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    /**
+     * solution III: using one marking variable
+     * @param matrix
+     */
+    public void setZeroesIII(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean flagCol0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                flagCol0 = true;
+            }
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (flagCol0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+}
