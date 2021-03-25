8d51c0b252e0d9141afbea11f2603475fdf51c50
bowei <bowei.han@cienet.com.cn>
2021-03-25 16:48:01 +0800

    add 82

---
+++ src/main/java/algorithms/medium/RemoveDuplicatesFromSortedListII82.java
@@ -0,0 +1,29 @@
+package algorithms.medium;
+
+import Bean.ListNode;
+
+/**
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii/
+ */
+public class RemoveDuplicatesFromSortedListII82 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
+}
