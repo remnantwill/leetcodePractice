d65911ba2c2154fa817c3f5d76d6002e87090018
bowei <bowei.han@cienet.com.cn>
2021-04-07 16:14:17 +0800

    add 81

---
+++ src/main/java/algorithms/medium/SearchInRotatedSortedArrayII81.java
@@ -0,0 +1,43 @@
+package algorithms.medium;
+
+/**
+ * https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/
+ */
+public class SearchInRotatedSortedArrayII81 {
+
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0;
+        int r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[l] == nums[mid] && nums[r] == nums[mid]) {
+                l++;
+                r--;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+}
