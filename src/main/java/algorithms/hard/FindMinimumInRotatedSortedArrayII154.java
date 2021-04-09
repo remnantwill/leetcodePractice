1e692c6b0ce7384e759ebebbb3502f8adc1810e6
bowei <bowei.han@cienet.com.cn>
2021-04-09 12:22:30 +0800

    add 154

---
+++ src/main/java/algorithms/hard/FindMinimumInRotatedSortedArrayII154.java
@@ -0,0 +1,24 @@
+package algorithms.hard;
+
+/**
+ * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/
+ */
+public class FindMinimumInRotatedSortedArrayII154 {
+
+    public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (nums[pivot] < nums[high]) {
+                high = pivot;
+            } else if (nums[pivot] > nums[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return nums[low];
+    }
+
+}
