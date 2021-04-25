fbbb58874da0a571704bc39e3cfbb6c09026c54b
remnantwill <remnantwill@163.com>
2021-04-25 18:42:57 +0800

    update 704

--- src/main/java/algorithms/easy/BinarySearch704.java
+++ src/main/java/algorithms/easy/BinarySearch704.java
@@ -5,6 +5,12 @@ package algorithms.easy;
  */
 public class BinarySearch704 {
 
+    /**
+     * solution I: binary search
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search(int[] nums, int target) {
         int len = nums.length;
         int left = 0;
@@ -17,6 +23,34 @@ public class BinarySearch704 {
                 left = mid + 1;
             } else {
                 right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * solution II: recursive
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchII(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        return recursion(left, right, nums, target);
+    }
+
+    private int recursion(int left, int right, int[] nums, int target) {
+        while (left <= right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+                return recursion(left, right, nums, target);
+            } else {
+                right = mid - 1;
+                return recursion(left, right, nums, target);
             }
         }
         return -1;
