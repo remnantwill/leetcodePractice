0e386d134375885c2277bdb892a724de7a8c0039
bowei <bowei.han@cienet.com.cn>
2021-05-14 16:50:14 +0800

    add 292

---
+++ src/main/java/algorithms/easy/NimGame292.java
@@ -0,0 +1,12 @@
+package algorithms.easy;
+
+/**
+ * https://leetcode-cn.com/problems/nim-game/
+ */
+public class NimGame292 {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+
+}
