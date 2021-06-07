ea662016e676d35c9be78317e05979b1e7a55213
bowei <bowei.han@cienet.com.cn>
2021-06-07 12:15:34 +0800

    add 494

---
+++ src/main/java/algorithms/medium/TargetSum494.java
@@ -0,0 +1,89 @@
+package algorithms.medium;
+
+/**
+ * https://leetcode-cn.com/problems/target-sum/
+ */
+public class TargetSum494 {
+
+    private int count;
+
+    /**
+     * solution I: backtracking
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWays(int[] nums, int target) {
+        backtrack(nums, target, 0, 0);
+        return count;
+    }
+
+    private void backtrack(int[] nums, int target, int index, int sum) {
+        if (index == nums.length) {
+            if (sum == target) {
+                count++;
+            }
+        } else {
+            backtrack(nums, target, index + 1, sum + nums[index]);
+            backtrack(nums, target, index + 1, sum - nums[index]);
+        }
+    }
+
+    /**
+     * solution II: dp
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWaysII(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int neg = diff / 2;
+        int[][] dp = new int[n + 1][neg + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int num = nums[i - 1];
+            for (int j = 0; j <= neg; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
+                }
+            }
+        }
+        return dp[n][neg];
+    }
+
+    /**
+     * solution II opt
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int findTargetSumWaysIIOpt(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int neg = diff / 2;
+        int[] dp = new int[neg + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = neg; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[neg];
+    }
+
+}
