b6c40c98bdc685d3739dc93daf7dc6337af01e75
bowei <bowei.han@cienet.com.cn>
2021-06-16 10:38:05 +0800

    add 877

---
+++ src/main/java/algorithms/medium/StoneGame877.java
@@ -0,0 +1,55 @@
+package algorithms.medium;
+
+/**
+ * https://leetcode-cn.com/problems/stone-game/
+ */
+public class StoneGame877 {
+
+    /**
+     * solution I: dp
+     * @param piles
+     * @return
+     */
+    public boolean stoneGame(int[] piles) {
+        int length = piles.length;
+        int[][] dp = new int[length][length];
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1] > 0;
+    }
+
+    /**
+     * solution I opt
+     * @param piles
+     * @return
+     */
+    public boolean stoneGameOpt(int[] piles) {
+        int length = piles.length;
+        int[] dp = new int[length];
+        for (int i = 0; i < length; i++) {
+            dp[i] = piles[i];
+        }
+        for (int i = length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < length; j++) {
+                dp[j] = Math.max(piles[i] - dp[j], piles[j] - dp[j - 1]);
+            }
+        }
+        return dp[length - 1] > 0;
+    }
+
+    /**
+     * solution II: math
+     * @param piles
+     * @return
+     */
+    public boolean stoneGameII(int[] piles) {
+        return true;
+    }
+
+}
