75ed11f20ab769e7ee8c20e28c97baa8cde394d0
bowei <bowei.han@cienet.com.cn>
2021-07-16 14:24:45 +0800

    add jianZhiOffer 53

---
+++ src/main/java/algorithms/easy/JianzhiOffer53.java
@@ -0,0 +1,55 @@
+package algorithms.easy;
+
+/**
+ * https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/
+ */
+public class JianzhiOffer53 {
+
+    /**
+     * solution I: binary search
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return rightIdx - leftIdx + 1;
+        }
+        return 0;
+    }
+
+    private int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * own solution
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchOwn(int[] nums, int target) {
+        int count = 0;
+        for (int num : nums) {
+            if (num == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
