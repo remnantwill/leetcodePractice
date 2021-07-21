c24b87b2fab088af318b1b9dac09e7821b18cd72
bowei <bowei.han@cienet.com.cn>
2021-07-21 11:37:27 +0800

    add jianzhiOffer 52

---
+++ src/main/java/algorithms/easy/JianzhiOffer52.java
@@ -0,0 +1,55 @@
+package algorithms.easy;
+
+import Bean.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+ */
+public class JianzhiOffer52 {
+
+    /**
+     * solution I: hash set
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    /**
+     * solution II: double pointer
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNodeII(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+}
