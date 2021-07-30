203f6bbe673db86ceba1715acadcf44ba94e3f70
bowei <bowei.han@cienet.com.cn>
2021-07-30 10:40:13 +0800

    add 171

---
+++ src/main/java/algorithms/easy/ExcelSheetColumnNumber171.java
@@ -0,0 +1,19 @@
+package algorithms.easy;
+
+/**
+ * https://leetcode-cn.com/problems/excel-sheet-column-number/
+ */
+public class ExcelSheetColumnNumber171 {
+
+    public int titleToNumber(String columnTitle) {
+        int number = 0;
+        int multiple = 1;
+        for (int i = columnTitle.length() - 1; i >= 0; i--) {
+            int k = columnTitle.charAt(i) - 'A' - 1;
+            number += k * multiple;
+            multiple *= 26;
+        }
+        return number;
+    }
+
+}
