44219182601803a9b5fa8fa368406ae89b0a0636
bowei <bowei.han@cienet.com.cn>
2021-09-06 17:23:27 +0800

    add 1588

--- src/main/java/algorithms/easy/SumOfAllOddLengthSubarrays1588.java
+++ src/main/java/algorithms/easy/SumOfAllOddLengthSubarrays1588.java
@@ -1,4 +1,68 @@
 package algorithms.easy;
 
+/**
+ * https://leetcode-cn.com/problems/sum-of-all-odd-length-subarrays/
+ */
 public class SumOfAllOddLengthSubarrays1588 {
+
+    /**
+     * solution I: force
+     * @param arr
+     * @return
+     */
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+        int n = arr.length;
+        for (int start = 0; start < n; start++) {
+            for (int length = 1; start + length <= n; length += 2) {
+                int end = start + length - 1;
+                for (int i = start; i <= end; i++) {
+                    sum += arr[i];
+                }
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * solution: prefix sum
+     * @param arr
+     * @return
+     */
+    public int sumOddLengthSubarraysII(int[] arr) {
+        int n = arr.length;
+        int[] prefixSums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefixSums[i + 1] = prefixSums[i] + arr[i];
+        }
+        int sum = 0;
+        for (int start = 0; start < n; start++) {
+            for (int length = 1; start + length <= n; length += 2) {
+                int end = start + length - 1;
+                sum += prefixSums[end + 1] - prefixSums[start];
+            }
+        }
+        return sum;
+    }
+
+    /**
+     * solution III: math
+     * @param arr
+     * @return
+     */
+    public int sumOddLengthSubarraysIII(int[] arr) {
+        int sum = 0;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int leftCount = i;
+            int rightCount = n - i - 1;
+            int leftOdd = (leftCount + 1) / 2;
+            int rightOdd = (rightCount + 1) / 2;
+            int leftEven = leftCount / 2 + 1;
+            int rightEven = rightCount / 2 + 1;
+            sum += arr[i] * (leftOdd * rightOdd + leftEven * rightEven);
+        }
+        return sum;
+    }
+
 }
--- src/test/java/algorithms/easy/SumOfAllOddLengthSubarrays1588Test.java
+++ src/test/java/algorithms/easy/SumOfAllOddLengthSubarrays1588Test.java
@@ -1,4 +1,37 @@
 package algorithms.easy;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@SpringBootTest(classes = SumOfAllOddLengthSubarrays1588.class)
+@RunWith(SpringRunner.class)
 public class SumOfAllOddLengthSubarrays1588Test {
+
+    @Resource
+    private SumOfAllOddLengthSubarrays1588 instance;
+
+    @Test
+    public void sumOddLengthSubarraysTest() {
+        int[] arr = new int[] { 1, 4, 2, 5, 3 };
+        int ans = instance.sumOddLengthSubarrays(arr);
+        System.out.println(ans);
+    }
+
+    @Test
+    public void sumOddLengthSubarraysTestII() {
+        int[] arr = new int[] { 1, 4, 2, 5, 3 };
+        int ans = instance.sumOddLengthSubarraysII(arr);
+        System.out.println(ans);
+    }
+
+    @Test
+    public void sumOddLengthSubarraysTestIII() {
+        int[] arr = new int[] { 1, 4, 2, 5, 3 };
+        int ans = instance.sumOddLengthSubarraysIII(arr);
+        System.out.println(ans);
+    }
 }
